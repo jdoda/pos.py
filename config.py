515817e07715a3573e18299c32e149f3b54efedb
Jonathan Doda <jonathan@jdoda.ca>
2011-01-14 21:04:32 -0500

    Move all configuration to config.py module

---
+++ config.py
@@ -0,0 +1,9 @@
+scanner_device_path = '/dev/input/by-id/usb-Metrologic_Metrologic_Scanner-event-kbd'
+database_uri = 'sqlite:///home/guest/pos.db'
+special_buttons = {
+    'Water'   :'055100300017', 
+    'Nestea'  :'839093', 
+    'Snickers':'058496814595',
+    'Coke'    :'678290', 
+    'Arizona' :'613008722579',
+}
