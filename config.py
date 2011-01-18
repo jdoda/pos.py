2e62239201cb2ad9b18825ac52e088320954de71
Jonathan Doda <jonathan@jdoda.ca>
2011-01-18 13:23:02 -0500

    Since it's public now, it should probably have a license. GPLv3 works.

--- config.py
+++ config.py
@@ -1,3 +1,19 @@
+# Copyright 2011 Jonathan Doda
+#
+# This file is part of pos.py.
+#
+# pos.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# pos.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pos.py.  If not, see <http://www.gnu.org/licenses/>.
+
 #scanner_device_path = '/dev/input/by-id/usb-Metrologic_Metrologic_Scanner-event-kbd'
 scanner_device_path = '/dev/null'
 database_uri = 'sqlite:///home/guest/pos.db'
--- report.py
+++ report.py
@@ -1,3 +1,19 @@
+# Copyright 2009 Ken Struys
+#
+# This file is part of pos.py.
+#
+# pos.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# pos.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pos.py.  If not, see <http://www.gnu.org/licenses/>.
+
 import db
 import csv
 
